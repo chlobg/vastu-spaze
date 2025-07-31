b3270d0a6450c3ea80e875d413fbc05b313d6300
chlobg <136447267+chlobg@users.noreply.github.com>
2025-07-31 16:42:12 +0700

    Enhance About component: add descriptive text to showcase VastuSpaze's mission and services; adjust Portfolio heading for improved spacing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,19 @@ const About = () => {
             className="w-full h-auto"
           />
         </div>
-        <p className="text-lg lg:text-3xl font-light text-center lg:text-left max-w-5xl mx-auto mt-8"></p>
+        <p className="text-lg lg:text-3xl font-light text-center lg:text-left max-w-5xl mx-auto mt-8">
+          At VastuSpaze, we believe in transforming houses into dream homes.
+          With a passion for design and a commitment to quality, our team of
+          skilled professionals has been dedicated to delivering exceptional
+          renovation services for over a decade. We specialize in creating
+          beautiful, functional spaces that reflect our clients unique tastes
+          and lifestyles. From modern kitchens and luxurious bathrooms to serene
+          outdoor living areas and cozy home offices, we bring our expertise to
+          every project, ensuring each renovation is tailored to meet your
+          specific needs and desires. Our mission is to enhance the beauty and
+          functionality of your home, making it a place where you can truly
+          thrive.
+        </p>
       </div>
     </section>
   );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,7 +4,7 @@ const Portfolio = () => {
   return (
     <section id="portfolio" className="max-w-7xl mx-auto border-b-2">
       <div className="mb-20">
-        <h2 className="text-xl lg:text-3xl tracking-light text-center uppercase mb-12">
+        <h2 className="text-xl lg:text-3xl tracking-tight text-center uppercase mt-20 mb-12">
           Portfolio
         </h2>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
